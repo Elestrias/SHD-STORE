2f23e05df36b13e43363e137d2836e577cf49ada
Sbrodov Egor <egor-ulstu@yandex.ru>
2019-03-28 16:22:22 +0400

    Added splitted views and message system

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from app import db
+from sqlalchemy.sql import func
 
 
 class User(db.Model):
@@ -6,6 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(50), unique=False, nullable=False)
+    unread = db.Column(db.Integer, default=0)
     role = db.Column(db.Integer, unique=False, nullable=False, default=1)
     hashcode = db.Column(db.String(50), unique=True)
 
@@ -45,6 +47,19 @@ class Product(db.Model):
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
 
     category = db.relationship('Category', backref=db.backref('products', lazy=True))
+
+
+class Message(db.Model):
+
+    __tablename__ = 'message'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    type = db.Column(db.Integer, default=1)
+    message = db.Column(db.String(500), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    user = db.relationship('User', backref=db.backref('messages', lazy=True))
 
 
 db.create_all()
