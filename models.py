1e545fdb3665ee99943c27502d7930557ff28b3a
Sbrodov Egor <egor-ulstu@yandex.ru>
2019-03-28 14:09:02 +0400

    Main pages done

--- models.py
+++ models.py
@@ -1,10 +1,9 @@
 from app import db
-from sqlalchemy.sql import expression
 
 
 class User(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(50), unique=False, nullable=False)
     role = db.Column(db.Integer, unique=False, nullable=False, default=1)
@@ -21,4 +20,38 @@ class User(db.Model):
         return '<User {}>'.format(self.email)
 
 
+class Category(db.Model):
+
+    __tablename__ = 'category'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), unique=True)
+    engname = db.Column(db.String(50), unique=True)
+    shown = db.Column(db.Integer, default=1)
+
+
+class Product(db.Model):
+
+    __tablename__ = 'product'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    description = db.Column(db.String(1000), unique=False)
+    count = db.Column(db.Integer, default=0)
+    delivery = db.Column(db.Integer, default=0)
+    image_url = db.Column(db.String(70), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    discount = db.Column(db.Integer, default=0)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+
+    category = db.relationship('Category', backref=db.backref('products', lazy=True))
+
+
 db.create_all()
+'''
+cat = Category.query.filter_by(id=1).first()
+prod = Product(name='Тапочки домашние', description='Lorem ipsum dolor sit amet', count=100, delivery=0,
+               image_url='http://asds.ru/1.jpg', price=2000, discount=50, category=cat)
+db.session.add(prod)
+db.session.commit()
+'''
--- view.py
+++ view.py
@@ -1,32 +1,36 @@
 from flask_mail import Message
 from app import app, mail
 from flask import render_template, session, request, redirect, url_for
-from models import db, User
+from models import db, User, Category, Product
 import hashlib
 from config import *
+
+
+def render(temp, **kwargs):
+    return render_template(temp, categories=Category.query.filter_by(shown=1).all(), **kwargs)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render('index.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
-        return render_template('register.html')
+        return render('register.html')
     elif request.method == 'POST':
         if request.form['email'] == '':
-            return render_template('register.html', email_empty=True)
+            return render('register.html', email_empty=True)
         if len(request.form['password']) < 6:
-            return render_template('register.html', pass_empty=True)
+            return render('register.html', pass_empty=True)
         if request.form['password'] != request.form['rpassword']:
-            return render_template('register.html', diff_pass=True)
+            return render('register.html', diff_pass=True)
         if not request.form.get('accept'):
-            return render_template('register.html', no_accept=True)
+            return render('register.html', no_accept=True)
         if User.query.filter_by(email=request.form['email']).first() is not None:
-            return render_template('register.html', user_exists=True)
+            return render('register.html', user_exists=True)
         password = hashlib.md5((SALT + request.form['password']).encode('utf-8')).hexdigest()
         hashcode = hashlib.md5((request.form['email'] + request.form['password']).encode('utf-8')).hexdigest()
         user = User(email=request.form['email'], password=password, hashcode=hashcode)
@@ -37,27 +41,27 @@ def register():
                       recipients=[request.form['email']])
         msg.body = 'Вы можете подтвердить свою учетную запись, перейдя по {}'.format(ADDRESS + 'confirm/' + hashcode)
         mail.send(msg)
-        return render_template('register.html', success=True)
+        return render('register.html', success=True)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+        return render('login.html')
     elif request.method == 'POST':
         if request.form['email'] == '':
-            return render_template('login.html', email_empty=True)
+            return render('login.html', email_empty=True)
         if request.form['password'] == '':
-            return render_template('login.html', pass_empty=True)
+            return render('login.html', pass_empty=True)
         user = User.query.filter_by(email=request.form['email']).first()
         if user is None:
-            return render_template('login.html', bad_data=True)
+            return render('login.html', bad_data=True)
         if user.password != hashlib.md5((SALT + request.form['password']).encode('utf-8')).hexdigest():
-            return render_template('login.html', bad_data=True)
+            return render('login.html', bad_data=True)
         if user.role == 0:
-            return render_template('login.html', banned=True)
+            return render('login.html', banned=True)
         if user.hashcode != '':
-            return render_template('login.html', unactivated=True)
+            return render('login.html', unactivated=True)
         session['email'] = request.form['email']
         session['password'] = hashlib.md5(request.form['password'].encode('utf-8')).hexdigest()
         return redirect(url_for('index'))
@@ -85,24 +89,60 @@ def confirm(hashcode):
 
 @app.route('/basket')
 def basket():
-    return render_template('basket.html')
+    products = [
+        {
+            'id': 1,
+            'count': 2
+        },
+        {
+            'id': 1,
+            'count': 2
+        },
+        {
+            'id': 1,
+            'count': 2
+        }
+    ]
+    summa = 0
+    for i in range(len(products)):
+        prod = Product.query.filter_by(id=products[i]['id']).first()
+        real_price = int(prod.price * (1 - prod.discount / 100))
+        products[i] = [prod, real_price, products[i]['count']]
+        summa += real_price * products[i][2]
+    return render('basket.html', products=products, sum=summa)
 
 
 @app.route('/lk-orders')
 def lk_orders():
-    return render_template('lk-orders.html')
+    return render('lk-orders.html')
 
 
 @app.route('/lk-msg')
 def lk_msg():
-    return render_template('lk-msg.html')
+    return render('lk-msg.html')
 
 
 @app.route('/lk-pass')
 def lk_pass():
-    return render_template('lk-pass.html')
+    return render('lk-pass.html')
 
 
 @app.route('/manager-panel')
 def manager_panel():
-    return render_template('manager-panel.html')
+    return render('manager-panel.html')
+
+
+@app.route('/category/<category>')
+def category(category):
+    cat = Category.query.filter_by(engname=category).first()
+    products = cat.products[:]
+    for i in range(len(products)):
+        products[i] = [products[i], int(products[i].price * (1 - products[i].discount / 100))]
+    return render('category.html', category=cat, products=products, len=len(products))
+
+
+@app.route('/product/<int:id>')
+def product(id):
+    prod = Product.query.filter_by(id=id).first()
+    real_price = int(prod.price * (1 - prod.discount / 100))
+    return render('product.html', product=prod, real_price=real_price)
